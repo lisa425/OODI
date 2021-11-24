ed740f0f491ed74bd5d16ea87051bb84283802b6
Im Chaewon <lisa425@sookmyung.ac.kr>
2021-11-24 10:03:33 +0900

    kakaomap address search

--- src/Components/common/AddressSetting.js
+++ src/Components/common/AddressSetting.js
@@ -4,46 +4,52 @@ import React,{useState,useEffect} from 'react';
 import '../../css/Components/Common/AddressSetting.css';
 import Back from './Back';
 import LargeButton from './LargeButton';
-
-const {kakao} = window
+import Map from './Map'
+// const {kakao} = window
 
 const AddressSetting = (props) => {
-    const [searchKeyword,setSearchKeyword] = useState('서울특별시 강동구 동남로 53길 20');
+    const [inputText,setInputText] = useState('')
+    const [searchKeyword,setSearchKeyword] = useState('');
     
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSearchKeyword(inputText)
+    }
 
-    useEffect(()=>{
-        const container = document.getElementById('map');
-		const options = {
-			center: new kakao.maps.LatLng(33.450701, 126.570667),
-			level: 3
-		};
-        const map = new kakao.maps.Map(container, options);
-        const marker = new kakao.maps.Marker({
-            position:map.getCenter()
-        })
-        marker.setMap(map);
+    // useEffect(()=>{
+    //     const container = document.getElementById('map');
+	// 	const options = {
+	// 		center: new kakao.maps.LatLng(33.450701, 126.570667),
+	// 		level: 3
+	// 	};
+    //     const map = new kakao.maps.Map(container, options);
+    //     const marker = new kakao.maps.Marker({
+    //         position:map.getCenter()
+    //     })
+    //     marker.setMap(map);
 
-        let geocoder = new kakao.maps.services.Geocoder();
-        const searchLocation = () => {
+    //     let geocoder = new kakao.maps.services.Geocoder();
+    //     const searchLocation = () => {
                 
-        }
-        geocoder.addressSearch(searchKeyword, function(result, status) {
-            // 정상적으로 검색이 완료됐으면 
-            if (status === kakao.maps.services.Status.OK) {
+    //     }
+    //     geocoder.addressSearch(searchKeyword, function(result, status) {
+    //         // 정상적으로 검색이 완료됐으면 
+    //         if (status === kakao.maps.services.Status.OK) {
 
-                let coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+    //             let coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
-                // 결과값으로 받은 위치를 마커로 표시합니다
-                let marker = new kakao.maps.Marker({
-                    map: map,
-                    position: coords
-                });
+    //             // 결과값으로 받은 위치를 마커로 표시합니다
+    //             let marker = new kakao.maps.Marker({
+    //                 map: map,
+    //                 position: coords
+    //             });
 
-                map.setCenter(coords);
-            } 
-        });    
+    //             map.setCenter(coords);
+    //         } 
+    //     });    
             
-    },[]);
+    // },[]);
+
 
    const setPropsAddress = () => {
        props.setOpenMap(false)
@@ -56,13 +62,16 @@ const AddressSetting = (props) => {
                 <Back link='/signup'/>
                 <h2>{props.title}</h2>
             </header>
-            <input 
-                type="text" 
-                value={searchKeyword} 
-                onChange={(e)=>setSearchKeyword(e.target.value)} 
-                className="search-keyword"
-            />
-            <div className="map" id="map"></div>
+            <section className="input-address">
+                <input 
+                    type="text" 
+                    value={inputText} 
+                    onChange={(e)=>setInputText(e.target.value)} 
+                    className="search-keyword"
+                />
+                <button className="submit-keyword" onClick={handleSubmit}>지도보기</button>
+            </section>
+            <Map searchKeyword={searchKeyword}/>
 
             <section className="confirm-address">
                 <h3>이 주소가 맞나요?</h3>
