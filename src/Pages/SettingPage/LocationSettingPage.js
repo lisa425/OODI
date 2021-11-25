cf8dea35028b59902cb7137879d17e19f41c801c
Im Chaewon <lisa425@sookmyung.ac.kr>
2021-11-26 03:13:49 +0900

    change location page

---
+++ src/Pages/SettingPage/LocationSettingPage.js
@@ -0,0 +1,12 @@
+import React,{useState} from 'react';
+import axios from 'axios';
+import AddressSetting from '../../Components/common/AddressSetting'
+const LocationSettingPage = () => {
+    return(
+        <main>
+            <AddressSetting />
+        </main>
+    )
+}
+
+export default LocationSettingPage;
