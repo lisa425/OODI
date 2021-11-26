c8ebe2e9ead93cb3a5dfe49730d6b6518645a0b4
Im Chaewon <lisa425@sookmyung.ac.kr>
2021-11-27 04:31:16 +0900

    distance popup layout

--- src/Components/search/DistancePopup.js
+++ src/Components/search/DistancePopup.js
@@ -1,9 +1,20 @@
 import React,{useState} from 'react';
+import LargeButton from '../common/LargeButton'
 
 const DistancePopup = (props) => {
     const [distance,setDistance] = useState(0)
+    const submitDistance = () => {
+        props.distance(distance);
+    }
     return(
-        <></>
+        <div className="popup">
+            <header className="popup-title">
+                <h3>거리</h3>
+                <CloseOutlined className="close" onClick={()=>props.setIsDistance(false)}/>
+            </header>
+            <div>반경 {distance}km 이내</div>
+            <LargeButton onClick={submitDistance}>설정하기</LargeButton>
+        </div>
     )
 }
 
