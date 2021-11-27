445f53ec414f5fd0d8773965afde556d13404d88
damda0904 <suen0904@gmail.com>
2021-11-27 14:22:03 +0900

    startDates => startDate

--- backend/controller/rsvController.js
+++ backend/controller/rsvController.js
@@ -3,12 +3,20 @@ import * as rsvRepository from '../data/reservation.js';
 export async function createReview(req, res) {
     const lessonId = req.params.classId;
     const ids = req.body.classTimeId;
+    const listSD = req.body.startDate
     const userId = req.userId;
 
     let lessonTimeId = ""
     for (var id of ids) {
         if (lessonTimeId == "") lessonTimeId = id.toString()
         else lessonTimeId = lessonTimeId + ", " + id.toString()
+    }
+
+    let startDate = ""
+    for (var date of listSD) {
+        if (startDate == "") startDate = date.toString();
+        else startDate = startDate + ", " + date.toString();
+
     }
 
     await rsvRepository.makeRsv({ userId, lessonId, lessonTimeId });
