9bfa7f96ff3e895b4fce4d500b774955f22eb3f0
Im Chaewon <lisa425@sookmyung.ac.kr>
2021-11-27 12:05:46 +0900

    get user info in confirm register

--- src/Pages/SearchPage/ConfirmRegister.js
+++ src/Pages/SearchPage/ConfirmRegister.js
@@ -3,6 +3,7 @@ import LargeButton from '../../Components/common/LargeButton';
 import '../../css/Pages/SearchPage/ConfirmRegister.css'
 import {Link} from 'react-router-dom';
 import DoneRegister from './DoneRegister'
+import axios from 'axios'
 
 const ConfirmRegister = (props) => {
     //예약 완료 팝업
@@ -12,10 +13,24 @@ const ConfirmRegister = (props) => {
     }
     
     useEffect(()=>{
+        const token = window.localStorage.getItem('TOKEN_KEY')
+        const config = {
+            headers:{"Authorization": `Bearer ${token}`}
+        };
 
+        //get user name
+        axios.get('http://localhost:8080/user',config)
+        .then(response => {
+            if(response.data.message === 'SUCCESS'){
+                console.log('success:',response.data)
+            }
+        }).catch((error)=>{
+            console.log('error:',error)
+        })
     },[])
     return (
         <main className="ConfirmRegister">
+            {/* <section className="" */}
             <LargeButton onClick={()=>handleDonePopup()}>결제하기</LargeButton>
             {doneRegister && <DoneRegister exercise={'원데이 맨몸필라테스'} daytime={['수요일 18:00~19:00','금요일 13:00~14:00']} setDoneRegister={setDoneRegister} setShowReservation={props.setShowReservation}/>}
         </main>
